fea4521d6e6255be8e74fd56183ee2a8d5a42084
Celeste Bareiro <cnyx7@proton.me>
2024-07-24 22:32:15 +0000

    learning about app.get and app.use

--- myApp.js
+++ myApp.js
@@ -1,29 +1,25 @@
 let express = require('express');
 let app = express();
 
+//console.log("Hello World");
 
+/*
+app.get('/', (req, res) => {
+    res.send('Hello Express');
+});
+*/
 
+app.get('/', (req, res) => {
+    let absolutePath = __dirname + '/views/index.html';
+    res.sendFile(absolutePath)
+})
 
+app.use('/public', express.static(__dirname + '/public'));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/public', (req, res) => {
+    let absolutePath = __dirname + '/public';
+    res.sendFile(absolutePath)
+})
 
 
 
